823073540d6f301be84c231c9a2f8b354c7c1c5f
mattjmorrison <mattjmorrison@mattjmorrison.com>
2013-10-31 22:02:10 -0500

    coffee!

---
+++ tests/tests.coffee
@@ -0,0 +1,2 @@
+test "simple test", ->
+    ok(something() == 100, "something didn't return 100")
--- tests/tests.js
+++
@@ -1,3 +0,0 @@
-test("simple test", function(){
-    ok(something() == 100, "something didn't return 100");
-});
